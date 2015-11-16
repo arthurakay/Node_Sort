74c1542f1e6538a5127cefe690605c3260f18d0e
Arthur Kay <arthurakay@gmail.com>
2015-11-16 15:34:57 -0600

    Additional logging, namely to announce a winner (and pct)

--- perf.js
+++ perf.js
@@ -1,6 +1,28 @@
 var js_sort = require('./sort.js'),
     c_sort = require('./c_module/build/Release/addon.node');
 
+var JS_AVG, CPP_AVG;
+
+function calculateWinner(num) {
+    var pct;
+
+    if (JS_AVG === CPP_AVG) {
+        console.log('\nBy some fluke of nature, JS and C++ took exactly the same amount of time to execute merge sort!');
+    }
+    else if (JS_AVG < CPP_AVG) {
+        pct = ((JS_AVG / CPP_AVG) - 1) * -100;
+
+        console.log('\nJS  Merge sort on ' + num + ' items is faster by pct: ' + pct);
+    }
+    else {
+        pct = ((CPP_AVG / JS_AVG) - 1) * -100;
+
+        console.log('\nC++ Merge sort on ' + num + ' items is faster by pct: ' + pct);
+    }
+
+    console.log('\n***\n');
+}
+
 function createArray(length) {
     var i           = 0,
         randomArray = [];
@@ -41,6 +63,7 @@ function return_JS_AverageTime(size) {
     }
 
     avg = totalTime / runs;
+    JS_AVG = avg;
 
     console.log('JS  Merge sort on ' + size + ' items took an average of ' + (avg * 1000) + ' ms (' + runs + ' runs).');
 
@@ -73,6 +96,7 @@ function return_C_AverageTime(size) {
     }
 
     avg = totalTime / runs;
+    CPP_AVG = avg;
 
     console.log('C++ Merge sort on ' + size + ' items took an average of ' + (avg * 1000) + ' ms (' + runs + ' runs).');
 
@@ -80,18 +104,22 @@ function return_C_AverageTime(size) {
 }
 
 var num = 20;
-
 return_JS_AverageTime(num);
 return_C_AverageTime(num);
+calculateWinner(num);
 
 
+num = 100;
+return_JS_AverageTime(num);
+return_C_AverageTime(num);
+calculateWinner(num);
 
-//return_JS_AverageTime(50);
-//return_JS_AverageTime(1000);
-//return_JS_AverageTime(10000);
+num = 1000;
+return_JS_AverageTime(num);
+return_C_AverageTime(num);
+calculateWinner(num);
 
-
-//return_C_AverageTime(50);
-//return_C_AverageTime(1000);
-//return_C_AverageTime(10000);
-
+num = 10000;
+return_JS_AverageTime(num);
+return_C_AverageTime(num);
+calculateWinner(num);
